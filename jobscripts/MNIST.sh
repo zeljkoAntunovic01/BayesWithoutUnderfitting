53661bdae7b36a9bdbd9d7ddd98e0d9e22053a40
Zeljko Antunovic <zeljko.ant123@gmail.com>
2025-05-07 10:40:30 +0200

    first HPC runs

---
+++ jobscripts/MNIST.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -J MNIST
+#BSUB -q gpua100
+#BSUB -W 12:00
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+
+#BSUB -o /zhome/4c/1/203176/BayesWithoutUnderfitting/hpc_logs/OUTPUT_FILE%J.out
+#BSUB -e /zhome/4c/1/203176/BayesWithoutUnderfitting/hpc_logs/OUTPUT_FILE%J.err
+
+module load python3/3.11.9
+
+source /zhome/4c/1/203176/BayesWithoutUnderfitting/venv/bin/activate
+python /zhome/4c/1/203176/BayesWithoutUnderfitting/main.py
