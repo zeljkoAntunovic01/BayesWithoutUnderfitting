5cc0c59030dbe509d20b0a0219ccdd70dd85be04
zeljkoAntunovic01 <zeljko.ant123@gmail.com>
2025-05-30 19:55:02 +0200

    added prints for allocated memory to debug CIFAR10

--- utils.py
+++ utils.py
@@ -478,6 +478,8 @@ def alternating_projections_qloss_classifier(
 
     precomputed_eigens = dict()
     precomputed_vjp_funs = dict()
+    print(f"Allocated memory before precomputation: {torch.cuda.memory_allocated() / (1024 ** 2):.2f} MB")
+    print(f"Reserved memory before precomputation: {torch.cuda.memory_reserved() / (1024 ** 2):.2f} MB")
     for i, (xb, yb) in enumerate(projection_data):
         xb, yb = xb.to(device), yb.to(device)
         eigvecs, inv_eigvals = precompute_loss_ggn_inverse(
@@ -488,6 +490,8 @@ def alternating_projections_qloss_classifier(
         
         precomputed_eigens[i] = (eigvecs, inv_eigvals)
         precomputed_vjp_funs[i] = vjp_fun
+        print(f"Allocated memory after batch {i}: {torch.cuda.memory_allocated() / (1024 ** 2):.2f} MB")
+        print(f"Reserved memory after batch {i}: {torch.cuda.memory_reserved() / (1024 ** 2):.2f} MB")
     
     print("✅ Precomputation complete.")
     print(f"Time taken for precomputation: {time.time() - precompute_ggn_eigvecs_time_start:.2f} seconds")
